b0e7b4cde350104e400e626aeba419159913e089
yaozhixing <yaozhixing797600@163.com>
2018-08-25 15:34:53 +0800

    生产环境

--- config/index.js
+++ config/index.js
@@ -49,9 +49,9 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/public/',
 
     /**
      * Source Maps
