7c401ec357447451f26ea2eadd8830d8dff79b67
yaozhixing <yaozhixing797600@163.com>
2018-08-27 23:37:51 +0800

    底部高亮

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import store from '../store'
 import axios from 'axios'
 
+let loadAllAxios = [];
+let loadCount = 0;
+
 axios.interceptors.request.use( (config) => {
+	//loadAllAxios.push("axiosarr" + (loadCount++));
 	store.dispatch("modifyLoadnFn",{ sta: true });
 	return config;
 })
@@ -11,5 +15,12 @@ axios.interceptors.response.use( (config) => {
 	store.dispatch("modifyLoadnFn",{ sta: false });
 	return config;
 })
+
+/*Promise.all(loadAllAxios).then((res) => {
+	console.log(loadCount);
+	store.dispatch("modifyLoadnFn",{ sta: false });
+}).catch( error => {
+	console.log(error)
+})*/
 
 export default axios
--- src/components/footbar.vue
+++ src/components/footbar.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="footbar" v-if="status">
-        <router-link to="/" class="fbar-item curr">
+        <router-link to="/" active-class="curr" class="fbar-item">
             <div class="dbico icon-home fbar-ico fbar-ico1"></div>
             <p class="fbar-title">首页</p>
         </router-link>
-        <router-link to="/hotmovie" class="fbar-item">
+        <router-link to="/hotmovie" active-class="curr" class="fbar-item">
             <div class="dbico icon-hot fbar-ico fbar-ico2"></div>
             <p class="fbar-title">正在热映</p>
         </router-link>
-        <router-link to="/soonmovie" class="fbar-item">
+        <router-link to="/soonmovie" active-class="curr" class="fbar-item">
             <div class="dbico icon-like fbar-ico fbar-ico3"></div>
             <p class="fbar-title">即将上映</p>
         </router-link>
-        <router-link to="/aboutus" class="fbar-item">
+        <router-link to="/aboutus" active-class="curr" class="fbar-item">
             <div class="dbico icon-my fbar-ico fbar-ico4"></div>
             <p class="fbar-title">关于我</p>
         </router-link>
